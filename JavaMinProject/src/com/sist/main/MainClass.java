9cff9ffe03963dac7edcdaa718ae27b919aa771b
microsoft <microsoft@sist>
2024-12-20 15:40:56 +0900

    2222

--- JavaMinProject/src/com/sist/main/MainClass.java
+++ JavaMinProject/src/com/sist/main/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
         System.out.println("test");
         System.out.println("11111");
-        
+        System.out.println("2222");
         
 	}
 	
